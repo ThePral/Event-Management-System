33603fb381be910e726db29433eb7d7ea83e93b1
Asalimasali <118522933+Asalimasali@users.noreply.github.com>
2025-01-20 20:33:32 +0330

    App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,14 +7,13 @@ import LoginSignup from './components/LoginSignup';
 import Forgetpass from './components/Forgetpass';
 import ResetCode from './components/ResetCode';
 import EditProf from './components/EditProf';
+import CreateEvent from './components/CreateEvent';
+import OurTeam from './components/OurTeam';
+import Dashboard from './components/Dashboard';
+import Events from './components/Events';
+import AdminPanel from './components/AdminPannel';
 
 
-
-// const App = () => {
-//   return (
-//     <Home />
-//   );
-// };
 
 function App() {
   return (
@@ -25,6 +24,11 @@ function App() {
         <Route path="/forgetpass" element={<Forgetpass />} />
         <Route path="/ResetCode" element={<ResetCode />} />
         <Route path="/EditProf" element={<EditProf />} />
+        <Route path="/CreateEvent" element={<CreateEvent />} />
+        <Route path="/OurTeam" element={<OurTeam />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Events" element={<Events />} />
+        <Route path="/AdminPannel" element={<AdminPanel />} />
 
       </Routes>
     </Router>
@@ -32,22 +36,3 @@ function App() {
 }
 
 export default App;
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './components/Home';
-// import LoginSignup from './components/LoginSignup';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<LoginSignup />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
